28ab8d346c0ec8febed02e29fbc6518b533ef9d4
Philip Kim <philipjwkim@gmail.com>
2023-09-23 10:09:53 -0700

    I've created a Node.js application with several features: It serves a list of phonebook entries, displays request timestamps and entry counts on a page, allows retrieval of single entries with error handling, enables entry deletion via HTTP DELETE requests, and supports adding new entries with validation to prevent missing or duplicate data.

---
+++ part3-exercises-3.1-3.6/index.js
@@ -0,0 +1,135 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+
+
+//* know this!
+/*
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
+*/
+
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(':method :url :status :response-time ms - :res[content-length] :body', {
+    skip: (req, res) => req.method !== 'POST', // Skip the 'body' token for non-POST requests
+  })
+)
+
+
+
+
+
+
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
+//* if you want to use line breaks, use backticks ``
+app.get('/info', (request, response) => {
+  const currentTime = new Date().toString()
+
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${currentTime}</p>
+  `)
+})
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+  ? Math.floor(Math.random() * 1000) + 1
+  : 0
+  return maxId + 1
+}
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({ //! calling return is crucial here otherwise, the note will end up getting added
+      error: 'content missing'
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {    //! NOTE: when testing in POSTMAN, change body content-type to JSON!
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+
+//! must specify PORT
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
