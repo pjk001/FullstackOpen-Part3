c77d4d09a31fa7febab4846357825fd1c3c7b81e
Philip Kim <philipjwkim@gmail.com>
2023-09-25 12:04:25 -0700

    Enabled the backend created in exercises 3.6-3.8 of Part3 to work with the phonebook frontend of Part-2/part2-exercises-app2.2 in the FullstackOpen repo. Deployed the backend to the internet and tested with Postman. Generated a production build of the frontend and added it to the internet application. Created a README which has a link to the online application.

---
+++ part3-exercises-3.9-3.11/README.md
@@ -0,0 +1,3 @@
+## Deployed Backend Application
+
+You can access the deployed backend application [here] (https://holy-cherry-8692.fly.dev/)
---
+++ part3-exercises-3.9-3.11/index.js
@@ -0,0 +1,145 @@
+const express = require('express') //imports express
+const morgan = require('morgan')  //imports morgan
+const cors = require('cors')  //imports cors
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(morgan('tiny'))
+
+
+//* know this!
+/*
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
+*/
+
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(':method :url :status :response-time ms - :res[content-length] :body', {
+    skip: (req, res) => req.method !== 'POST', // Skip the 'body' token for non-POST requests
+  })
+)
+
+
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Temporary Root</h1>')
+})
+
+
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
+//* if you want to use line breaks, use backticks ``
+app.get('/info', (request, response) => {
+  const currentTime = new Date().toString()
+
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${currentTime}</p>
+  `)
+})
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+//adding
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({ //! calling return is crucial here otherwise, the note will end up getting added
+      error: 'content missing'
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {    //! NOTE: when testing in POSTMAN, change body content-type to JSON!
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+
+//deleting
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+
+//functionality for updating numbers in exercise 3.17
+//  HERE...
+
+
+
+
+
+//! must specify PORT
+//* NOTE: for Fly.io and Render, we need to change the port definition like so:
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
